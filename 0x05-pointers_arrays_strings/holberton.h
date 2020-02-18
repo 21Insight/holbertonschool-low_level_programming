2483fe6043ab46b03d4b598c9ba60a33a937a8ab
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-02-18 02:22:55 -0500

    Project development exercise 0

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter & updates the value
+ * @n: pointer to int n
+ */
+
+void reset_to_98(int *n);
+
+#endif
