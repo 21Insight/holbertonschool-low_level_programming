00163de9726861b16db1e70807a3bb9aaa7114c1
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-02-18 22:48:47 -0500

    Project development exercise 9

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -65,4 +65,13 @@ void puts2(char *str);
 
 void puts_half(char *str);
 
+/**
+ * _strcpy - copies the string pointed to by src,including the terminating null
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ */
+
+char *_strcpy(char *dest, char *src);
+
 #endif
