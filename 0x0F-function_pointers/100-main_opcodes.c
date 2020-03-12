c76c4f50f67742173bbd2d3ae4bb38e2383191ec
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-03-12 04:40:26 +0000

    [Project development]: 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
